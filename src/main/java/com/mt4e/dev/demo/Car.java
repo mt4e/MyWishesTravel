1c4775550f2289af96755c1ebdccb9f43049835b
Nacim <naciim25@gmail.com>
2018-09-04 23:51:22 +0200

    added server and client module

---
+++ src/main/java/com/mt4e/dev/demo/Car.java
@@ -0,0 +1,21 @@
+package com.mt4e.dev.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.*;
+
+
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+@Entity
+@Getter @Setter
+public class Car {
+
+  @Id
+  @GeneratedValue
+  private int id;
+
+  private @NotNull String name;
+}
