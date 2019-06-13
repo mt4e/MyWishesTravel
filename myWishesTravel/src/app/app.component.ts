6bbe017ef28edcd5a69abea9902fc6dc75599721
Nacim <naciim25@gmail.com>
2019-06-13 22:23:44 +0100

    correction leaflet css import

--- myWishesTravel/src/app/app.component.ts
+++ myWishesTravel/src/app/app.component.ts
@@ -9,13 +9,16 @@ import * as L from 'leaflet';
 export class AppComponent implements OnInit {
   title = 'myWishesTravel';
 
-// Fonction d'initialisation du composant.
-ngOnInit() {
-  // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
-  const myMap = L.map('myMap').setView([50.6311634, 3.0599573], 12);
+  // Fonction d'initialisation du composant.
+  ngOnInit() {
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    const myMap = L.map('myMap').setView([45.747091, 4.924944], 17);
 
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: 'My map'
-  }).addTo(myMap);
-}
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+    }).addTo(myMap);
+
+    var marker = L.marker([45.747091, 4.924944]).addTo(myMap);
+    marker.bindPopup("<b>Sweet home.").openPopup();
+  }
 }
