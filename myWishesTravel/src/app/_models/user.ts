b4777ce702f1878f814dfb23375f6d504b5a79e1
Nacim <naciim25@gmail.com>
2019-07-29 22:58:34 +0100

    moved web site in a new repo

--- myWishesTravel/src/app/_models/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    id: number;
-    userName: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    token: string;
-}
--- myWishesTravel/src/app/_services/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './user.service';
-export * from './alert.service';
-export * from './authentication.service';
